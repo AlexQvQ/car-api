fcfebef956e5436d8b5561bbf50612f7bc705cc6
AlexQvQ <aleksandrnelson.1900@gmail.com>
2025-08-15 05:09:39 +0300

    remove bugs

--- database/migrations/2025_08_11_210915_create_vacancies_table.php
+++ database/migrations/2025_08_11_210915_create_vacancies_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('vacancies', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->timestamps();
             $table->foreignIdFor(User::class);
         });
--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/', function (Request $request) {
-//     return 'hi bitch';
-// });
 Route::middleware('auth:sanctum')->get('/', [CarController::class, 'index']);
 
 
